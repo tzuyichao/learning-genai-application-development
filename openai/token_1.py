489057b0fad8674d7ba80c5afbe751f2bf952a62
TzuYi Chao <tzuyichao0313@gmail.com>
2024-06-01 03:30:01 +0000

    token lab 1

--- openai/token_1.py
+++ openai/token_1.py
@@ -1,10 +1,23 @@
 import tiktoken
 
+def encode(encoder, text):
+    tokens = encoder.encode(text)
+    print(tokens)
+    print(encoder.decode(tokens))
+    print('-' * 10)
+
 encoder = tiktoken.encoding_for_model('gpt-3.5-turbo')
 print(encoder.name)
+
+encode(encoder, "你好")
 
 encoder = tiktoken.encoding_for_model('gpt-4')
 print(encoder.name)
 
+encode(encoder, "你好")
+
+
 encoder = tiktoken.encoding_for_model('gpt-4o')
 print(encoder.name)
+
+encode(encoder, "你好")
